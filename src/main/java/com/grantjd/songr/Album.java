fba482ddf9674a4e549272f53ddf97276c9a6f9d
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-23 13:23:58 -0800

    lab 12 complete

--- src/main/java/com/grantjd/songr/Album.java
+++ src/main/java/com/grantjd/songr/Album.java
@@ -1,22 +1,56 @@
 package com.grantjd.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
-    public String title;
-    public String img;
-    public String artist;
-//    public int length;
-//    public int releaseDate;
-    public String genre;
-    public int songCount;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String title;
+    private String img;
+    private String artist;
+    private int length;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    private int songCount;
 
 
-    public Album(String title, String img, String artist, String genre, int songCount) {
+    public Album(String title, String img, String artist, int length, int songCount) {
         this.title = title;
         this.img = img;
         this.artist = artist;
-//        this.length = length;
-//        this.releaseDate = releaseDate;
-        this.genre = genre;
+        this.length = length;
         this.songCount = songCount;
     }
+
+    public Album(){}
 }
---
+++ src/main/java/com/grantjd/songr/AlbumRepository.java
@@ -0,0 +1,7 @@
+package com.grantjd.songr;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+}
