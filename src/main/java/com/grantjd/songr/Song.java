26705e256fb066b0f7cb92ef9d4ebb591d59cdbb
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-23 16:36:01 -0800

    some functionality complete, pages linked, add a sond to db not done, set up fill structure for css

--- src/main/java/com/grantjd/songr/Song.java
+++ src/main/java/com/grantjd/songr/Song.java
@@ -1,11 +1,36 @@
 package com.grantjd.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private String title;
     private int songLength;
     private int trackNumber;
     private String album;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
 
     public Song(String title, int songLength, int trackNumber, String album) {
         this.title = title;
